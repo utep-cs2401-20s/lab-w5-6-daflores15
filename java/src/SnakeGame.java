d14401d194e62db75b5a4333448e564aaa23dc12
danny <daflores15@miners.utep.edu>
2020-02-19 14:44:56 -0700

    setting up SnakeGame, I will be attending office hours because i need more help.

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -1,2 +1,45 @@
 public class SnakeGame {
+    private boolean[][] game;
+    private int[] headPosition = new int[2]; //(x,y)
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+    int x, y;
+    int length;
+
+    SnakeGame(){
+        int[][] gameBoard = new int[1][1];
+    }
+
+    SnakeGame(boolean[][] headPosition, int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int findTailExhaustive (int x, int y, int lenght) {
+        //find the tail by searching across the whole grid
+
+    }
+
+    public int findTailRecursive () {
+        //find the tail using recursion
+
+    }
+
+    private int findTailRecursive(int[] currentPosition, int[] previousPosition){
+
+    }
+
+    private void resetCounters() {
+
+    }
+
+    private static int getRecursiveChecks() {
+
+    }
+
+    private static int getExhaustiveChecks() {
+
+    }
+
 }
+
